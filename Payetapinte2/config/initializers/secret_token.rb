17c48afc3af4e5a6c2467f3d3f26f5b5f307f267
alexben <alexiben7@gmail.com>
2014-02-16 14:51:35 +0100

    setup rails project

---
+++ Payetapinte2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Payetapinte2::Application.config.secret_token = '77f4490331f24ea2b1d41a969873a0eedf5c7319abf31b6bbd96331d07deeafdfec31a558b0a55857470c1dc9e50e6f7d5329a01b6018aeb062c733fdbf620ca'
